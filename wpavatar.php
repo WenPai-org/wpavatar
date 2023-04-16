a0f5406a7023698a2fd90137e2a57679ab14c043
feng <f2010525@gmail.com>
2023-04-16 23:50:36 +0800

    功能更新

--- wpavatar.php
+++ wpavatar.php
@@ -2,13 +2,12 @@
 /**
  * Plugin Name: WPAvatar
  * Plugin URI: https://wpavatar.com/download
- * Description: Must-have for WordPress sites in China, showing your ICP license.
+ * Description: Replace Gravatar with Cravatar, a perfect replacement of Gravatar in China.
  * Author: WPfanyi
  * Author URI: https://wpfanyi.com/
  * Text Domain: wpavatar
  * Domain Path: /languages
  * Version: 1.0
- * Network: True
  * License: GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  *
@@ -23,44 +22,23 @@
  * GNU General Public License for more details.
  */
 
- if ( ! function_exists( 'get_cravatar_url' ) ) {
-     /**
-      * 替换 Gravatar 头像为 Cravatar 头像
-      *
-      * Cravatar 是 Gravatar 在中国的完美替代方案，你可以在 https://cravatar.cn 更新你的头像
-      */
-     function get_cravatar_url( $url ) {
-         $sources = array(
-             'www.gravatar.com',
-             '0.gravatar.com',
-             '1.gravatar.com',
-             '2.gravatar.com',
-             'secure.gravatar.com',
-             'cn.gravatar.com',
-             'gravatar.com',
-         );
-         return str_replace( $sources, 'cravatar.cn', $url );
-     }
-     add_filter( 'um_user_avatar_url_filter', 'get_cravatar_url', 1 );
-     add_filter( 'bp_gravatar_url', 'get_cravatar_url', 1 );
-     add_filter( 'get_avatar_url', 'get_cravatar_url', 1 );
- }
- if ( ! function_exists( 'set_defaults_for_cravatar' ) ) {
-     /**
-      * 替换 WordPress 讨论设置中的默认头像
-      */
-     function set_defaults_for_cravatar( $avatar_defaults ) {
-         $avatar_defaults['gravatar_default'] = 'Cravatar 标志';
-         return $avatar_defaults;
-     }
-     add_filter( 'avatar_defaults', 'set_defaults_for_cravatar', 1 );
- }
- if ( ! function_exists( 'set_user_profile_picture_for_cravatar' ) ) {
-     /**
-      * 替换个人资料卡中的头像上传地址
-      */
-     function set_user_profile_picture_for_cravatar() {
-         return '<a href="https://cravatar.cn" target="_blank">您可以在 Cravatar 修改您的资料图片</a>';
-     }
-     add_filter( 'user_profile_picture_description', 'set_user_profile_picture_for_cravatar', 1 );
- }
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly
+}
+
+
+require_once( plugin_dir_path( __FILE__ ) . 'includes/cravatar.php' );
+
+
+register_activation_hook( __FILE__, 'wpavatar_activate' );
+
+function wpavatar_activate() {
+  update_option( 'wpavatar_enable_cravatar', '1' );
+}
+
+
+// Load translation
+add_action( 'init', 'wpavatar_load_textdomain' );
+function wpavatar_load_textdomain() {
+	load_plugin_textdomain( 'wpavatar', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
+}
